bcda4e9b6db008de51c974eb1b069c5099d94b14
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-07 13:46:18 +0300

    adds code for python p3 user

---
+++ main.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, name):
+        print("User.__init__ called.")
+        self.name = name
+
+    def log_in(self):
+        self.logged_in = True
+
+
+class Student(User):
+    def __init__(self, name, grade):
+        print("Student.__init__ called.")
+        super().__init__(name)
+        self.grade = grade
+
+    def log_in(self):
+        super().log_in()
+        self.in_class = True
+
+
+oneil = Student("Seth", "A-")
+print(oneil.__dict__)
